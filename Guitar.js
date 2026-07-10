682c0590ea27f79db9f4d0e22091ab61fe8efde3
Gamehenge <liamcash10@gmail.com>
2026-07-09 21:40:50 -0700

    added pentatonic scales

--- Guitar.js
+++ Guitar.js
@@ -1,31 +1,36 @@
-const chromaticScale = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-const majSeq = [2, 2, 1, 2, 2, 2, 1];
-const minSeq = [2, 1, 2, 2, 1, 2, 2]
 
+const chromaticScale = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+
+const majSeq = [2, 2, 1, 2, 2, 2, 1];
+const minSeq = [2, 1, 2, 2, 1, 2, 2];
+
+const minPent = [3, 2, 2, 2, 2];
+const majPent = [2, 2, 3, 2, 3];
+
+
+
+/**
+ * Maps out string notes on fretboard 
+ * given string tuning (openNote) and scale length (numFrets)
+ * param {*} openNote 
+ * param {*} numFrets 
+ */
 function getFretNotes(openNote, numFrets) {
   const startIndex = chromaticScale.indexOf(openNote);
   const notes = [];
 
   for (let fret = 0; fret <= numFrets; fret++) {
-    const noteIndex = (startIndex + fret) % chromaticScale.length;
+    const noteIndex = (startIndex + fret) % chromaticScale.length; // restarts note sequence if > 12 notes 
     notes.push(chromaticScale[noteIndex]);
   }
 
   return notes;
 }
 
-const numFrets = 12;
 
-const strings = [
-  getFretNotes("E", numFrets), // low E string
-  getFretNotes("A", numFrets), // A string
-  getFretNotes("D", numFrets), // D string
-  getFretNotes("G", numFrets), // G string
-  getFretNotes("B", numFrets), // B string
-  getFretNotes("E", numFrets), // high e string
-];
-
-
+/**
+ * Prints fretboard grid and all respective notes
+ */
 function printNeck() {
   const stringsReversed = [...strings].reverse(); // high e on top
 
@@ -46,6 +51,14 @@ function printNeck() {
   });
 }
 
+
+/**
+ * Prints all notes associated with 
+ * a given scale and a given root on entire neck
+ * 
+ * param {*} root 
+ * param {*} type 
+ */
 function printScale(root, type) {
   let seq = []; 
   switch(type) {
@@ -55,11 +68,17 @@ function printScale(root, type) {
       case "minor":
           seq = minSeq;
           break;
+      case "major pentatonic":
+          seq = majPent;
+          break;
+      case "minor pentatonic":
+          seq = minPent;
+          break;
       default:
           break;
   }
 
-   const startIndex = chromaticScale.indexOf(root);
+  const startIndex = chromaticScale.indexOf(root);
   const notes = [];
 
   let curInterval = 0;
@@ -90,10 +109,20 @@ function printScale(root, type) {
     });
     console.log(row);
   });
-
-
 }
 
+const numFrets = 12;
+
+const strings = [
+  getFretNotes("E", numFrets), // low E string
+  getFretNotes("A", numFrets), // A string
+  getFretNotes("D", numFrets), // D string
+  getFretNotes("G", numFrets), // G string
+  getFretNotes("B", numFrets), // B string
+  getFretNotes("E", numFrets), // high e string
+];
+
+
 printNeck();
-printScale("C", "minor"); 
+printScale("G", "minor pentatonic"); 
 
